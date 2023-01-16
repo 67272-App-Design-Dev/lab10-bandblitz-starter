d2d23546d284aff4ced000239cae1a42246f0a8c
robloxosf <ayoubman@yahoo.com>
2023-01-15 19:09:13 -0500

    Update lab to upgraded Rails/Ruby

--- db/schema.rb
+++ db/schema.rb
@@ -2,38 +2,37 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925172344) do
-
+ActiveRecord::Schema[7.0].define(version: 2017_09_25_172344) do
   create_table "band_genres", force: :cascade do |t|
     t.integer "band_id"
     t.integer "genre_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "bands", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "playing_next"
-    t.datetime "when_playing_next"
+    t.datetime "when_playing_next", precision: nil
     t.string "photo"
     t.string "song"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
 end
